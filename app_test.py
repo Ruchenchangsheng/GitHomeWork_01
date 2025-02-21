eafd42252057ad45eed89cd172bdbab49412aeb7
Koovvr <koovvr@gmail.com>
2025-02-21 16:20:37 +0300

    Fix flake8 issues

--- app_test.py
+++ app_test.py
@@ -1,6 +1,7 @@
-from app import *
+from app import add
+
 
 def test_app():
-    assert add(1,2) == 3
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+    assert add(1, 2) == 3
+    assert add(-1,  1) == 0
+    assert add(0,  0) == 0
