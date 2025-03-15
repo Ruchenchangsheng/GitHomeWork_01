5de108610564bdce02396296881661eb55a918bd
Koovvr <koovvr@gmail.com>
2025-03-16 02:52:59 +0300

    test7

--- app_test.py
+++ app_test.py
@@ -7,7 +7,7 @@ class add_test(unittest.TestCase):
         self.assertEqual(add(1, 2), 3)
 
     def test_add2(self):
-        self.assertEqual(add(2, 2), 4)
+        self.assertEqual(add(3, 2), 5)
 
 
 if __name__ == '__main__':
